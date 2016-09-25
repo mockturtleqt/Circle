b961bd1af13b29cf98d6d4642580e6ed313ac3f6
mockturtleqt <galia.semashko@gmail.com>
2016-09-25 12:30:02 +0300

    Tests added.

---
+++ src/com/epam/training/first/CircleTest.java
@@ -0,0 +1,17 @@
+package com.epam.training.first;
+import com.epam.training.first.exception.NotCircleException;
+import com.epam.training.first.shapes.Circle;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+//tests are created in folder "test" but it won't upload to github. so this is a copy
+public class CircleTest {
+    @Test
+    public void isCircle(){
+        //not sure if necessary because object with radius <= 0 will not be built
+        try {
+            Circle c1 = new Circle.CircleBuilder(2).build();
+            assertTrue(c1.getRadius() > 0);
+        } catch (NotCircleException e) {}
+    }
+}
